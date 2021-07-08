e1c8f9235ac688de4a372e171ae6a42b9f0e4a91
JKCTech <jeffreykcorporation@live.nl>
2021-07-08 14:39:07 +0200

    Terminal upgrades & Fixes

--- Tools/terminal.py
+++ Tools/terminal.py
@@ -4,8 +4,8 @@ import os
 
 # Check args
 if len(sys.argv) != 2:
-    print("Please provide a serial port to connect to.")
-    exit(1)
+	print("Please provide a serial port to connect to.")
+	exit(1)
 
 # Get serial connection & Open
 ser = userial.get(sys.argv[1])
@@ -13,26 +13,36 @@ ser.open()
 
 # Loop
 while True:
-    cmds = input(">> ").upper().split(';')
-    
-    for cmd in cmds:
-    
-        cmd = cmd.strip()
+	cmds = input(">> ").upper().split(';')
+	
+	for cmd in cmds:
+	
+		cmd = cmd.strip()
 
-        # Custom commands
-        # Clear
-        if (cmd == "CLEAR" or cmd == "CLS"):
-            if os.name == 'posix':
-                os.system('clear')
-            else:
-                os.system('cls')
-            continue
+		# Custom commands
+		# Clear
+		if (cmd == "CLEAR" or cmd == "CLS"):
+			if os.name == 'posix':
+				os.system('clear')
+			else:
+				os.system('cls')
+			continue
+		
+		elif (len(cmd) > 3 and cmd[0:5] == "KEYS,"):
+			chars = cmd[5:]
 
-        # Exit
-        elif (cmd == "EXIT"):
-            userial.command(ser, "EPG")
-            ser.close()
-            break
+			for c in chars:
+				userial.command(ser, "KEY,{},P".format(c))
+			continue
 
-        # Execute command & Print result
-        print(userial.command(ser, cmd))
+		# Exit
+		elif (cmd == "EXIT"):
+			try:
+				userial.command(ser, "EPG")
+				ser.close()
+			except Exception:
+				pass
+			exit(0)
+
+		# Execute command & Print result
+		print(userial.command(ser, cmd))
